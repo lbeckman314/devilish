905c7073614e2227326bf6687ce6ae90869123c4
lbeckman314 <lbeckman314@gmail.com>
2018-07-13 12:13:01 -0700

    updated jenkins build file

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@ production=/var/www/pkgs/$project
 src=/var/www/pkgs/$project/src
 
 #tar -zcvf $project.tar.gz $project
-git archive --format=tar -v -o $project.tar.gz $project
+git archive --format=tar -v -o $project.tar.gz HEAD
 #zip -r $project.zip $project
-git archive --format=zip -v -o $project.zip $project
+git archive --format=zip -v -o $project.zip HEAD
 sha256sum *.tar.gz *.zip > sha256sums.txt
 gpg --pinentry-mode loopback --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
 
