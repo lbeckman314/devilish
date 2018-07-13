0461fb566b58ae69939ddfefdb61e90716a747fa
lbeckman314 <lbeckman314@gmail.com>
2018-07-13 11:50:19 -0700

    added jenkins build file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# https://support.cloudbees.com/hc/en-us/articles/203802500-Injecting-Secrets-into-Jenkins-Build-Jobs
+set +x
+
+project=devilish
+production=/var/www/pkgs/$project/src
+
+tar -zcvf $project.tar.gz $project
+zip -r $project.zip $project
+sha256sum *.tar.gz *.zip > sha256sums.txt
+gpg --pinentry-mode loopback --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
+
+mv $project.tar.gz $project.zip sha256sums.txt* $production
+
