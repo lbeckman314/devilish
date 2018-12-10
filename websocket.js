5bde51df97604efa583869c85a7b4fecfe7fb0c7
lbeckman314 <lbeckman314@gmail.com>
2018-12-10 15:31:43 -0800

    updated websocket client

--- websocket.js
+++ websocket.js
@@ -10,25 +10,11 @@ process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 // Create WebSocket connection.
 const socket = new WebSocket('wss://192.168.0.15:8181');
 
-console.log("Success!");
-
-
 // Connection opened
 socket.onopen = function (event) {
+	console.log("Success!");
 	//command = readline.question(": ");
 	console.log("Sending initial message to server.");
-	socket.send("");
-}
-
-
-// Listen for messages
-socket.onmessage = function(event) {
-	console.log(event.data);
-
-	//let rl = require('readline-sync');
-	//let comm = rl.question(': ');
-	//socket.send(comm);
-
 
 	const rl = readline.createInterface({
 		input: process.stdin,
@@ -40,9 +26,26 @@ socket.onmessage = function(event) {
 		// TODO: Log the answer in a database
 		socket.send(answer);
 
-		rl.close();
 	});
+
+	// Listen for messages
+	socket.onmessage = function(event) {
+		console.log(event.data);
+
+		//let rl = require('readline-sync');
+		//let comm = rl.question(': ');
+		//socket.send(comm);
+
+		readline.moveCursor(process.stdout, 0, -1);
+		rl.question(': ', (answer) => {
+			// TODO: Log the answer in a database
+			socket.send(answer);
+
+			//rl.close();
+		});
+	}
 }
+
 
 
 
