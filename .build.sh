6372dc1228071f7d7fdea3792858f96622fdc5b2
lbeckman314 <lbeckman314@gmail.com>
2018-12-18 17:31:51 -0800

    updated build file

--- .build.sh
+++ .build.sh
@@ -11,6 +11,7 @@ obj=devilish.out
 git archive --format=tar -v -o $project.tar.gz HEAD
 #zip -r $project.zip $project
 git archive --format=zip -v -o $project.zip HEAD
+
 sha256sum *.tar.gz *.zip > sha256sums.txt
 gpg --pinentry-mode loopback --passphrase $gpgpass --batch --yes --detach-sign -a sha256sums.txt
 
